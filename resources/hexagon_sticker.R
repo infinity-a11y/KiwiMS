6bbabbe6858bae9b66ebfa4946ddade987eb9608
infinity-a11y <marian-freisleben@outlook.com>
2025-04-30 22:02:48 +0200

    Logo border color changes

--- resources/hexagon_sticker.R
+++ resources/hexagon_sticker.R
@@ -1,8 +1,10 @@
+# github logo
+
 hexSticker::sticker(
   package = "KiwiFlow",
   subplot = "resources/kiwi.png",
   h_fill = "#E8CB98",
-  h_color = "black",
+  h_color = "#36367B",
   s_width = 0.8,
   s_height = 0.8,
   s_y = 1.15,
@@ -10,6 +12,5 @@ hexSticker::sticker(
   p_color = "black",
   p_y = 0.5,
   p_family = "Consolas", p_size = 5.5,
-  filename = "app/static/github_logo.svg", 
-  white_around_sticker = T
+  filename = "app/static/github_logo.svg"
 )
