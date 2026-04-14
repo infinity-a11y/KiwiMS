aa811df48438b5864498eca0451d747e1cfbcabf
infinity-a11y <marian-freisleben@outlook.com>
2026-04-14 15:27:00 +0200

    Adapted deconvolution test script to result file changes

--- setup/deconvolution_test.ps1
+++ setup/deconvolution_test.ps1
@@ -51,7 +51,8 @@ try {
     Write-Output "Executing R deconvolution logic..."
     
     # We pass the specific $testTempDir to the R script
-    & $condaCmd run -n $envName --no-capture-output Rscript.exe "$basePath\app\logic\deconvolution_execute.R" $testTempDir $testTempDir $basePath $testTempDir "testing"
+    $dbPath = Join-Path $testTempDir "results.db"
+    & $condaCmd run -n $envName --no-capture-output Rscript.exe "$basePath\app\logic\deconvolution_execute.R" $testTempDir $testTempDir $basePath $testTempDir "testing" $dbPath "FALSE"
 
     # Remove config file
     Remove-Item -Path "$basePath\resources\config.rds", (Join-Path $testTempDir "config.rds") -Force -ErrorAction SilentlyContinue
